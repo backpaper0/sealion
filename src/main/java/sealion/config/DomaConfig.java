0638fb93fa054786434ff1889d9df4a1a4de42bf
Taichi Uragami <backpaper0@gmail.com>
2016-01-12 21:56:31 +0900

    TODOリストアプリケーションを作りたい

---
+++ src/main/java/sealion/config/DomaConfig.java
@@ -0,0 +1,33 @@
+package sealion.config;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.sql.DataSource;
+
+import org.seasar.doma.jdbc.Config;
+import org.seasar.doma.jdbc.dialect.Dialect;
+import org.seasar.doma.jdbc.dialect.H2Dialect;
+
+@ApplicationScoped
+public class DomaConfig implements Config {
+
+    @Resource
+    private DataSource dataSource;
+    private Dialect dialect;
+
+    @PostConstruct
+    public void init() {
+        dialect = new H2Dialect();
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Override
+    public Dialect getDialect() {
+        return dialect;
+    }
+}
---
+++ src/main/java/sealion/config/JaxrsConfig.java
@@ -0,0 +1,10 @@
+package sealion.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationScoped
+@ApplicationPath("")
+public class JaxrsConfig extends Application {
+}
