3e6df59ec14a8bb4b29da1e9eec83bb36094b5ae
Taichi Uragami <backpaper0@gmail.com>
2016-01-14 21:40:19 +0900

    Enum.nameを利用するのでドメインクラスでなくても良い

--- src/main/java/sealion/domain/AccountRole.java
+++ src/main/java/sealion/domain/AccountRole.java
@@ -1,17 +1,6 @@
 package sealion.domain;
 
-import org.seasar.doma.Domain;
-
-@Domain(valueType = String.class, factoryMethod = "of")
 public enum AccountRole {
 
-    USER, ADMIN;
-
-    public String getValue() {
-        return name();
-    }
-
-    public static AccountRole of(String value) {
-        return valueOf(value);
-    }
+    USER, ADMIN
 }
