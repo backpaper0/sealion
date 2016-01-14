e48c5b9171cdc0fa43a5f8051a1858114fce3ceb
Taichi Uragami <backpaper0@gmail.com>
2016-01-14 21:31:40 +0900

    FKを付けた

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,6 +1,33 @@
+CREATE TABLE Account (
+    id IDENTITY,
+    username VARCHAR(50)
+);
+
+CREATE TABLE Grant (
+    account BIGINT NOT NULL,
+    role VARCHAR(10) NOT NULL,
+    PRIMARY KEY (account, role),
+    FOREIGN KEY (account) REFERENCES Account (id)
+);
+
+CREATE TABLE Password (
+    account BIGINT NOT NULL,
+    hash VARCHAR(200) NOT NULL,
+    salt VARCHAR(200) NOT NULL,
+    hashAlgorithm VARCHAR(50) NOT NULL,
+    PRIMARY KEY (account),
+    FOREIGN KEY (account) REFERENCES Account (id)
+);
+
 CREATE TABLE Project (
     id IDENTITY,
     name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Milestone (
+    id IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    fixedDate TIMESTAMP
 );
 
 CREATE TABLE Task (
@@ -11,7 +38,10 @@ CREATE TABLE Task (
     postedBy BIGINT NOT NULL,
     postedAt TIMESTAMP NOT NULL,
     project BIGINT NOT NULL,
-    Milestone BIGINT NOT NULL
+    Milestone BIGINT NOT NULL,
+    FOREIGN KEY (postedBy) REFERENCES Account (id),
+    FOREIGN KEY (project) REFERENCES Project (id),
+    FOREIGN KEY (Milestone) REFERENCES Milestone (id)
 );
 
 CREATE TABLE Comment (
@@ -19,36 +49,15 @@ CREATE TABLE Comment (
     task BIGINT NOT NULL,
     content VARCHAR(1000) NOT NULL,
     postedBy BIGINT NOT NULL,
-    postedAt TIMESTAMP NOT NULL
-);
-
-CREATE TABLE Account (
-    id IDENTITY,
-    username VARCHAR(50)
-);
-
-CREATE TABLE Password (
-    account BIGINT NOT NULL,
-    hash VARCHAR(200) NOT NULL,
-    salt VARCHAR(200) NOT NULL,
-    hashAlgorithm VARCHAR(50) NOT NULL,
-    PRIMARY KEY (account)
+    postedAt TIMESTAMP NOT NULL,
+    FOREIGN KEY (postedBy) REFERENCES Account (id),
+    FOREIGN KEY (task) REFERENCES Task (id)
 );
 
 CREATE TABLE Assignment (
     task BIGINT NOT NULL,
     account BIGINT NOT NULL,
-    PRIMARY KEY (task, account)
-);
-
-CREATE TABLE Grant (
-    account BIGINT NOT NULL,
-    role VARCHAR(10) NOT NULL,
-    PRIMARY KEY (account, role)
-);
-
-CREATE TABLE Milestone (
-    id IDENTITY,
-    name VARCHAR(100) NOT NULL,
-    fixedDate TIMESTAMP
+    PRIMARY KEY (task, account),
+    FOREIGN KEY (task) REFERENCES Task (id),
+    FOREIGN KEY (account) REFERENCES Account (id)
 );
