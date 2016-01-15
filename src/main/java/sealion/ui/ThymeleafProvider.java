cad69926a8b5abd72554b830b5bc6bf4fb59130c
Taichi Uragami <backpaper0@gmail.com>
2016-01-16 07:38:28 +0900

    Thymeleafを使う準備をする

---
+++ src/main/java/sealion/ui/ThymeleafProvider.java
@@ -0,0 +1,78 @@
+package sealion.ui;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@ApplicationScoped
+@Provider
+public class ThymeleafProvider implements ContainerResponseFilter, MessageBodyWriter<UIResponse> {
+
+    private MediaType contentType;
+    private TemplateEngine templateEngine;
+
+    @PostConstruct
+    public void init() {
+        String charset = StandardCharsets.UTF_8.name();
+        contentType = MediaType.TEXT_HTML_TYPE.withCharset(charset);
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCacheable(false);
+        templateResolver.setCharacterEncoding(charset);
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+            ContainerResponseContext responseContext) throws IOException {
+        if (responseContext.getEntityType() == UIResponse.class) {
+            responseContext.getHeaders().putSingle(HttpHeaders.CONTENT_TYPE, contentType);
+        }
+    }
+
+    @Override
+    public void writeTo(UIResponse t, Class<?> type, Type genericType, Annotation[] annotations,
+            MediaType mediaType, MultivaluedMap<String, Object> httpHeaders,
+            OutputStream entityStream) throws IOException, WebApplicationException {
+        Context context = new Context();
+        context.setVariable("model", t.model);
+        try (Writer out = new OutputStreamWriter(entityStream, StandardCharsets.UTF_8)) {
+            templateEngine.process(t.template, context, out);
+        }
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations,
+            MediaType mediaType) {
+        return true;
+    }
+
+    @Override
+    public long getSize(UIResponse t, Class<?> type, Type genericType, Annotation[] annotations,
+            MediaType mediaType) {
+        return -1;
+    }
+}
---
+++ src/main/java/sealion/ui/UIResponse.java
@@ -0,0 +1,20 @@
+package sealion.ui;
+
+public final class UIResponse {
+
+    public final String template;
+    public final Object model;
+
+    private UIResponse(String template, Object model) {
+        this.template = template;
+        this.model = model;
+    }
+
+    public static UIResponse render(String template, Object model) {
+        return new UIResponse(template, model);
+    }
+
+    public static UIResponse render(String template) {
+        return new UIResponse(template, null);
+    }
+}
