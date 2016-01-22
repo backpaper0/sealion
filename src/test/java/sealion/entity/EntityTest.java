441198e789121bf29c175872af883ab7a2e34025
Taichi Uragami <backpaper0@gmail.com>
2016-01-23 06:15:02 +0900

    テスト対象のエンティティクラスを検出するようにした

--- src/test/java/sealion/entity/EntityTest.java
+++ src/test/java/sealion/entity/EntityTest.java
@@ -2,8 +2,18 @@ package sealion.entity;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+import java.util.Spliterators;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import javax.sql.DataSource;
 
@@ -14,6 +24,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
+import org.seasar.doma.Entity;
 import org.seasar.doma.jdbc.Config;
 import org.seasar.doma.jdbc.builder.SelectBuilder;
 import org.seasar.doma.jdbc.dialect.Dialect;
@@ -62,15 +73,42 @@ public class EntityTest {
 
     @Parameters(name = "{0}")
     public static List<Class<?>> parameters() throws Exception {
-        List<Class<?>> entityClasses = new ArrayList<>();
-        entityClasses.add(Account.class);
-        entityClasses.add(Assignment.class);
-        entityClasses.add(Comment.class);
-        entityClasses.add(Grant.class);
-        entityClasses.add(Milestone.class);
-        entityClasses.add(Password.class);
-        entityClasses.add(Project.class);
-        entityClasses.add(Task.class);
-        return entityClasses;
+
+        Path classpathRoot = Paths
+                .get(Task.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+
+        //テーブルに1対1の関係でマッピングされているエンティティクラスが
+        //格納されているディレクトリを検出しています。
+        Path entityDir = classpathRoot.resolve(Task.class.getPackage().getName().replace('.', '/'));
+
+        Function<Path, String> toFQCN = file -> {
+            String s = file.toString();
+            s = s.substring(0, s.length() - ".class".length());
+            s = s.replace('/', '.');
+            return s;
+        };
+
+        Function<String, Optional<Class<?>>> tryToClass = className -> {
+            try {
+                return Optional.of(Class.forName(className));
+            } catch (ClassNotFoundException e) {
+                return Optional.empty();
+            }
+        };
+
+        Predicate<Class<?>> isEntity = clazz -> clazz.isAnnotationPresent(Entity.class);
+
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(entityDir)) {
+
+            //IteratorをもとにしてStreamを構築
+            Stream<Path> stream = StreamSupport
+                    .stream(Spliterators.spliteratorUnknownSize(ds.iterator(), 0), false);
+
+            List<Class<?>> entityClasses = stream.map(classpathRoot::relativize).map(toFQCN)
+                    .map(tryToClass).flatMap(a -> a.map(Stream::of).orElseGet(Stream::empty))
+                    .filter(isEntity).collect(Collectors.toList());
+
+            return entityClasses;
+        }
     }
 }
