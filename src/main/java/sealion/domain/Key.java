03d42fc93f78620e628347fdfdcd375a36e38eb5
Taichi Uragami <backpaper0@gmail.com>
2016-02-07 07:45:35 +0900

    Key.valueOfの存在意義をコメントで記載

--- src/main/java/sealion/domain/Key.java
+++ src/main/java/sealion/domain/Key.java
@@ -23,6 +23,15 @@ public class Key<ENTITY> {
         return value.toString();
     }
 
+    /*
+     * このメソッドはJAX-RSのパラメーターでKeyクラスを使用するために定義しています。
+     * 
+     * JAX-RSではString型の引数を1つだけもつvalueOfという名前のstaticファクトリーメソッドが
+     * あるクラスを@QueryParamや@FormParamで受け取るパラーメーターとして使えます。
+     * 
+     * 詳しくは次のURLを参照してください。
+     * http://backpaper0.github.io/2013/07/17/jaxrs_parameter.html
+     */
     public static <ENTITY> Key<ENTITY> valueOf(String value) {
         return new Key<>(Long.valueOf(value));
     }
