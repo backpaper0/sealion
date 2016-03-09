5e30f413e453e883abe52eeff5a7a79b28f28153
Taichi Uragami <backpaper0@gmail.com>
2016-03-09 23:08:47 +0900

    拡張子をもとにContent-Typeを設定する

--- src/main/java/sealion/ui/WebJarsResources.java
+++ src/main/java/sealion/ui/WebJarsResources.java
@@ -2,11 +2,15 @@ package sealion.ui;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
@@ -15,13 +19,24 @@ import javax.ws.rs.ext.Provider;
 @PreMatching
 public class WebJarsResources implements ContainerRequestFilter {
 
+    private Map<String, MediaType> extension;
+
+    @PostConstruct
+    public void init() {
+        Map<String, MediaType> extension = new HashMap<>();
+        extension.put(".js", MediaType.valueOf("application/javascript"));
+        extension.put(".css", MediaType.valueOf("text/css"));
+        this.extension = extension;
+    }
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         String path = requestContext.getUriInfo().getPath();
         if (path.startsWith("webjars/")) {
             InputStream in = getClass().getResourceAsStream("/META-INF/resources/" + path);
             if (in != null) {
-                Response response = Response.ok(in).build();
+                MediaType mediaType = extension.get(path.substring(path.lastIndexOf('.')));
+                Response response = Response.ok(in, mediaType).build();
                 requestContext.abortWith(response);
             }
         }
